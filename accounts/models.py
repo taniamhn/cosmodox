da1c646b88aef107a00226340dc77c13abbc2888
Tania Melissa Hernandez Narvaez <taniamhn@gmail.com>
2018-10-12 17:22:16 -0500

    custom user manager and admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db import models
+from .managers import UserManager
 
 
 class User(AbstractUser):
 
     username = None
     email = models.EmailField(unique=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
